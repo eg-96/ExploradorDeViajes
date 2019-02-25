16e604be304c5508911ea7dbe207a555b1f46fc7
Enmanuel García <egarciag@ucenfotec.ac.cr>
2019-02-24 20:18:37 -0600

    Avance en funcionalidad de búsqueda de vuelos

--- app/src/main/java/com/example/exploradordeviajes/FlySearching.java
+++ app/src/main/java/com/example/exploradordeviajes/FlySearching.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.SearchView;
 
 import com.example.exploradordeviajes.Adapters.OneWayFlightAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class FlySearching extends AppCompatActivity {
@@ -32,21 +34,61 @@ public class FlySearching extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         simpleFlightView.setLayoutManager(layoutManager);
 
-        // Read from the database
-        db.collection("Flights")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            QuerySnapshot results = task.getResult();
+        //Default search
+        executeSearch("", "");
 
-                            oneWayFlightAdapter = new OneWayFlightAdapter(results);
-                            simpleFlightView.setAdapter(oneWayFlightAdapter);
-                        } else {
-                            Log.w("Error", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+        SearchView searchByDeparture = findViewById(R.id.searchByDestination);
+        searchByDeparture.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String value) {
+                executeSearch("DepartureCityName", value);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String value) {
+                executeSearch("DepartureCityName", value);
+                return true;
+            }
+        });
+
+        SearchView searchByDestination = findViewById(R.id.searchByDestination);
+        searchByDestination.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String value) {
+                executeSearch("DestinationCityName", value);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String value) {
+                executeSearch("DestinationCityName", value);
+                return true;
+            }
+        });
+    }
+
+    // Read from the database
+    private void executeSearch(String field, String value){
+        Query query;
+
+        if (field.isEmpty())
+            query = db.collection("Flights");
+        else
+            query = db.collection("Flights").whereEqualTo(field, value);
+
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            if (task.isSuccessful()) {
+                QuerySnapshot results = task.getResult();
+
+                oneWayFlightAdapter = new OneWayFlightAdapter(results);
+                simpleFlightView.setAdapter(oneWayFlightAdapter);
+            } else {
+                Log.w("Error", "Error getting documents.", task.getException());
+            }
+            }
+        });
     }
 }
