ff26fd7b9a964cc8fe9d9c794814b9a9842375d3
Josue Guerrero <josue.guerrero@interaction.cr>
2019-03-29 17:13:10 -0600

    background, cardview

---
+++ app/src/main/java/com/example/exploradordeviajes/Compra.java
@@ -0,0 +1,13 @@
+package com.example.exploradordeviajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Compra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compra);
+    }
+}
